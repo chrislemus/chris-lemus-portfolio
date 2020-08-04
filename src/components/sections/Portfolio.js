19d7bb6eb826e516688d3c4ba1efbfec3eed129d
Chris Lemus <icrislemus@gmail.com>
2020-08-04 14:25:32 -0400

    project data and project cards modified

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -1,65 +1,15 @@
 import React, { Component } from 'react';
-import {Modal} from 'react-bootstrap'
 import projectsDB from '../../data/projectsData';
-// import thumbnail from '../../imgs/portfolio/employee-directory/ed-thumbnail.png';
-// import ProjectLightbox from '../partials/ProjectLightbox';
 
 
 export default class Portfolio extends Component{
 
-  state ={
-    displayLightbox: false,
-    projectName: null,
-    projectDesc: null,
-    projectGithub: null,
-    projectDemo: null
-  }
-
   render() {
-    const {
-      displayLightbox,
-      projectName,
-      projectDesc,
-      // projectGithub,
-      // projectDemo
-    } = this.state;
 
     return (
-      <section className="portfolio content">
+      <section className="portfolio">
 
         <h2>Portfolio</h2>
-
-      <Modal show={displayLightbox} animation={false} onHide={this.handleClose}>
-        <Modal.Header closeButton>
-            <h4>{projectName}</h4>
-        </Modal.Header>
-        <Modal.Body>
-          <p className="modal-project-description">{projectDesc}</p>
-          <div className="modal-project-tech">
-            <h6>TECHNOLOGIES</h6>
-            {this.getTechIcons()}
-          </div>
-
-          <div className="project-modal-btn-container">
-            <button className="github-link project-modal-links" onClick={this.handleClose}>
-              GitHub Repo
-            </button>
-            <button className="project-demo-link" onClick={this.handleClose}>
-              Live Demo
-            </button>
-          </div>
-          
-        </Modal.Body>
-        {/* <Modal.Footer>
-          <Button variant="secondary" onClick={this.handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={this.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer> */}
-      </Modal>
-
 
         <div className="portfolio--subhead">
           <p>Creating a digital product is full of rewards and challenges.</p>
@@ -76,12 +26,9 @@ export default class Portfolio extends Component{
   getTechIcons = () => {
     const project = projectsDB.find(x => x.id === 1 );
     let happy = []
-    // project.technologies.forEach(icon => {
-    //     happy.push(<img key={icon.index} src={icon} alt=""/>)
-    //   })
     project.technologies.forEach(getImgs)
     function getImgs(img, index, arr) {
-      happy.push(<img key={arr[index]} src={img} alt="tech-icon"/>)
+      happy.push(<img id="project-tech-icons" key={arr[index]} src={img} alt="tech-icon"/>)
     }
       
     return happy
@@ -117,48 +64,25 @@ export default class Portfolio extends Component{
 
   getThumbnails = () => projectsDB.map( (project) => {
     return (  
-        <div key={project.id} className="project--card" onClick={() => this.handleShow(project.id)}>
+        <div key={project.id} className="project--card">
             <img 
                     src={project.thumbnail.default}
                     alt="project-thumbnail"/>
-            <div className="project--card--text">
-              <h6>{project.project_name}</h6>
-            </div>
+              <div className="project--card--text">
+                <h6 className="project-name-text">{project.project_name}</h6>
+                <p>{project.description} </p>
+                <h6 className="project-tech-header">Technologies</h6>
+                <div>
+                  {this.getTechIcons()}
+                </div>
+                <div className="project-card-buttons">
+                  <a href={project.github_repo} target="_blank" projectid={project.id}>Github Repo</a>
+                  <a href={project.live_demo_url} target="_blank" projectid={project.id}>Live Demo</a>
+                </div>
+              </div>
         </div>                 
     )
   })
 
-  // getThumbnails = () => projectsDB.map( (project) => {
-  //   return (  
-  //       <div key={project.id} className="project--card">
-  //           <img 
-  //                   src={project.thumbnail.default}
-  //                   alt="project-thumbnail"/>
-  //             <div className="project--card--text">
-  //               <h6>{project.project_name}</h6>
-  //               <hr className="divider--project--card"/>
-  //               <p>{project.description} </p>
-  //               <a id="learn--more--project--link" onClick={this.handleShow} projectid={project.id}>Learn More</a>
-  //             </div>
-  //       </div>                 
-  //   )
-  // })
-
    
  }
-
-
-//  <Modal.Dialog className="project--lightbox">
-//  <Modal.Header closeButton>
-//    <Modal.Title>Modal title</Modal.Title>
-//  </Modal.Header>
-
-//  <Modal.Body>
-//    <p>Modal body text goes here.</p>
-//  </Modal.Body>
-
-//  <Modal.Footer>
-//    <Button variant="secondary">Close</Button>
-//    <Button variant="primary">Save changes</Button>
-//  </Modal.Footer>
-// </Modal.Dialog>
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,62 +4,110 @@ const htmlIcon = require("../imgs/portfolio/tech-icons/icon-html.png")
 const projects = [
         {
             "id": 1,
-            "project_name": "Random Quote Generator",
+            "project_name": "Online Courses WebApp",
             "description": "An app that generates a random quote from a list of quotes with a click of a button.",
-            "live_demo_url": "http://chrislemus.io/FSJS-project-1/",
-            "github_repo": "https://github.com/chrislemus/FSJS-project-1",
-            "thumbnail": require("../imgs/portfolio/thumbnails/project-1-thumbnail.png"),
-            "screenshot": ["/static/img/project-1-1.png", "/static/img/project-1-2.png"],
+            "live_demo_url": "https://webcourses.netlify.app/",
+            "github_repo": "https://github.com/chrislemus/online-courses-webapp",
+            "thumbnail": require("../imgs/portfolio/thumbnails/p1-thumb.jpg"),
             "technologies": [htmlIcon, cssIcon]
         },
         {
             "id": 2,
             "project_name": "Employee Directory",
             "description": "Employee directory that communicates with a third-party API (Application Programming Interface).",
-            "live_demo_url": "http://chrislemus.io/FSJS-project-1/",
-            "github_repo": "https://github.com/chrislemus/FSJS-project-1",
-            "thumbnail": require("../imgs/portfolio/employee-directory/ed-thumbnail.png"),
-            "screenshot": [require("../imgs/portfolio/employee-directory/ed-ss1.png"), require("../imgs/portfolio/employee-directory/ed-ss2.png")],
+            "live_demo_url": "https://chrislemus.github.io/Employee-Directory",
+            "github_repo": "https://github.com/chrislemus/Employee-Directory",
+            "thumbnail": require("../imgs/portfolio/thumbnails/p2-thumb.jpg"),
             "technologies": ["HTML", "CSS", "JavaScript"]
         },
         {
             "id": 3,
-            "project_name": "Random Quote Generator",
+            "project_name": "Library Books Database",
             "description": "An app that generates a random quote from a list of quotes with a click of a button.",
-            "live_demo_url": "http://chrislemus.io/FSJS-project-1/",
-            "github_repo": "https://github.com/chrislemus/FSJS-project-1",
-            "thumbnail": require("../imgs/portfolio/thumbnails/project-2-thumbnail.png"),
-            "screenshot": ["/static/img/project-1-1.png", "/static/img/project-1-2.png"],
+            "live_demo_url": "https://library-db-sql.herokuapp.com/books",
+            "github_repo": "https://github.com/chrislemus/Library-Books-DB",
+            "thumbnail": require("../imgs/portfolio/thumbnails/p3-thumb.jpg"),
             "technologies": ["HTML", "CSS", "JavaScript"]
         },
         {
             "id": 4,
-            "project_name": "Random Quote Generator",
+            "project_name": "Analytics Dashboard",
             "description": "An app that generates a random quote from a list of quotes with a click of a button.",
-            "live_demo_url": "http://chrislemus.io/FSJS-project-1/",
-            "github_repo": "https://github.com/chrislemus/FSJS-project-1",
-            "thumbnail": require("../imgs/portfolio/thumbnails/project-3-thumbnail.png"),
-            "screenshot": ["/static/img/project-1-1.png", "/static/img/project-1-2.png"],
+            "live_demo_url": "https://chrislemus.github.io/Analytics-Dashboard/",
+            "github_repo": "https://github.com/chrislemus/Analytics-Dashboard",
+            "thumbnail": require("../imgs/portfolio/thumbnails/p4-thumb.jpg"),
             "technologies": ["HTML", "CSS", "JavaScript"]
         },
         {
             "id": 5,
-            "project_name": "Random Quote Generator",
+            "project_name": "Online Gallery",
             "description": "An app that generates a random quote from a list of quotes with a click of a button.",
-            "live_demo_url": "http://chrislemus.io/FSJS-project-1/",
-            "github_repo": "https://github.com/chrislemus/FSJS-project-1",
+            "live_demo_url": "https://gallery-app-react.netlify.app/",
+            "github_repo": "https://github.com/chrislemus/React-Gallery-App",
             "thumbnail": require("../imgs/portfolio/thumbnails/project-4-thumbnail.png"),
-            "screenshot": ["/static/img/project-1-1.png", "/static/img/project-1-2.png"],
             "technologies": ["HTML", "CSS", "JavaScript"]
         },
         {
             "id": 6,
+            "project_name": "OOP Game Show App",
+            "description": "An app that generates a random quote from a list of quotes with a click of a button.",
+            "live_demo_url": "https://chrislemus.github.io/OOP-Game-Show-App/",
+            "github_repo": "https://github.com/chrislemus/OOP-Game-Show-App",
+            "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
+            "technologies": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "id": 7,
+            "project_name": "Static Node Express Site",
+            "description": "An app that generates a random quote from a list of quotes with a click of a button.",
+            "live_demo_url": "https://static-node-express-site.herokuapp.com/",
+            "github_repo": "https://github.com/chrislemus/static-node-express-site",
+            "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
+            "technologies": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "id": 8,
+            "project_name": "Directory List Pagination",
+            "description": "An app that generates a random quote from a list of quotes with a click of a button.",
+            "live_demo_url": "https://chrislemus.github.io/directory-list-pagination/",
+            "github_repo": "https://github.com/chrislemus/directory-list-pagination",
+            "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
+            "technologies": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "id": 9,
+            "project_name": "Interaction Form",
+            "description": "An app that generates a random quote from a list of quotes with a click of a button.",
+            "live_demo_url": "https://chrislemus.github.io/Interactive-Form/",
+            "github_repo": "https://github.com/chrislemus/Interactive-Form",
+            "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
+            "technologies": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "id": 10,
             "project_name": "Random Quote Generator",
             "description": "An app that generates a random quote from a list of quotes with a click of a button.",
-            "live_demo_url": "http://chrislemus.io/FSJS-project-1/",
-            "github_repo": "https://github.com/chrislemus/FSJS-project-1",
+            "live_demo_url": "https://chrislemus.github.io/Random-Quote-Generator",
+            "github_repo": "https://github.com/chrislemus/Random-Quote-Generator",
             "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
-            "screenshot": ["/static/img/project-1-1.png", "/static/img/project-1-2.png"],
+            "technologies": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "id": 11,
+            "project_name": "Web Style Guide",
+            "description": "An app that generates a random quote from a list of quotes with a click of a button.",
+            "live_demo_url": "https://chrislemus.github.io/web-style-guide-sass/",
+            "github_repo": "https://github.com/chrislemus/web-style-guide-sass",
+            "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
+            "technologies": ["HTML", "CSS", "JavaScript"]
+        },
+        {
+            "id": 12,
+            "project_name": "Online Registration",
+            "description": "An app that generates a random quote from a list of quotes with a click of a button.",
+            "live_demo_url": "https://chrislemus.github.io/online-registration-form/",
+            "github_repo": "https://github.com/chrislemus/online-registration-form",
+            "thumbnail": require("../imgs/portfolio/thumbnails/project-5-thumbnail.png"),
             "technologies": ["HTML", "CSS", "JavaScript"]
         }
     ]
