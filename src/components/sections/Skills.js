c79c963c997e4c1375d06431bf0fa7e55a92b5e0
Chris Lemus <icrislemus@gmail.com>
2020-08-12 17:06:20 -0400

    added my skills text content

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -42,7 +42,65 @@ export default class Skills extends Component{
                                 defaultActiveKey={tabSelected}
                                 onSelect={this.tabClick.bind(this)}>
                                 
-                                {this.getTabs()}
+                                <Tab eventKey="Code" title="Code" key="1">
+                                    <p>
+                                        Full Stack Javascript Developer. Creating responsive and fast WebApps. More info on this site 
+                                        <a href="https://developers.google.com/speed/pagespeed/insights/?url=chrislemus.io" target="_blank" rel="noopener noreferrer"> View PageSpeed</a> | 
+                                        <a href="https://github.com/chrislemus/chris-lemus-portfolio" target="_blank" rel="noopener noreferrer"> View Github Repo </a>
+                                    </p>
+                                    <p>
+                                        <ul>
+                                            <li>Express</li>
+                                            <li>Node JS</li>
+                                            <li>NPM</li>
+                                            <li>MySQL</li>
+                                            <li>Bootstrap and other front-end frameworks</li>
+                                            <li>HTML5 stack</li>
+                                            <li>CSS3</li>
+                                        </ul>
+                                    </p>
+                                </Tab>
+
+
+                                <Tab eventKey="Business" title="Business" key="2">
+                                    <p>
+                                        As someone who started two local service businesses, I will use my experience to deliver a 
+                                        product that is tailored to your project/business needs. *check out businesses below
+                                    </p>
+                                    <ul>
+                                        <li>
+                                            <p>
+                                                <a href="https://www.maidsnextdoor.com/" target="_blank" rel="noopener noreferrer">
+                                                    Maids Next Door</a>: extensive use of data analytics for automatic scheduling, dispatch, and pricing
+                                            </p>
+                                        </li>
+                                        <li>
+                                            <p><a href="https://ncpropainters.com/" target="_blank" rel="noopener noreferrer">
+                                                NC Pro Painters</a>: Managing large scale projects/crews, making sure everything is on schedule. And service is delivered to customer as promised.</p></li>
+                                    </ul>    
+                                </Tab>
+
+
+                                <Tab eventKey="Media" title="Media" key="3">
+                                    <p>
+                                        In depth knowledge of Video/Image formats: Through my freelance work I have developed skills with PhotoShop, Premiere Pro,
+                                         After Effects, and other Adobe Suite programs. Video has proven to help with branding and increase site conversion rate. Check out my latest projects below.
+                                    </p>
+                                    <iframe src="https://fast.wistia.net/embed/iframe/sizbswz78p" allow="autoplay; fullscreen"  frameBorder="0" allowFullScreen width="45%" height="45%" style={{paddingRight: "8px"}}/>
+                                    <iframe src="https://player.vimeo.com/video/389623594" width="50%" height="50%" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen/>  
+                                </Tab>
+
+
+                                <Tab eventKey="Marketing" title="Marketing" key="4">
+                                    <p>If content is King and distribution is Queen, why work on a project that people have a hard time finding? 
+                                        With my experience of online SEO, I will make sure your project follows the search engine guidelines.
+                                        This will give your project a better chance at appearing on top of search engine results for your audience to find.
+                                    </p>
+                                    <p>
+                                        <a href="https://www.google.com/search?q=maids+next+door" target="_blank" rel="noopener noreferrer">Click here </a> 
+                                        for example of Maids Next Door
+                                    </p>
+                                </Tab>
                             </Tabs>
                         </Col>
                     </Row>
