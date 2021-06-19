1233316da086363e840d72aa4e48bffad1a2a79c
Chris Lemus <icrislemus@gmail.com>
2021-06-19 11:20:36 -0400

    update image logo file path in navbar

--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import { 
-    Nav,
-    Navbar,
- } from 'react-bootstrap';
- import logo from '../../imgs/icons/logo.png';
-
+import { Nav, Navbar } from 'react-bootstrap';
 
 export default () => {
-
   return (
     <section id="navbar">
-      <Navbar expand="lg"  >
+      <Navbar expand="lg">
         <Navbar.Brand href="/">
-          <img
-            src={logo}
-            alt="site-logo"
-          />
+          <img src="./images/icons/logo.png" alt="site-logo" />
         </Navbar.Brand>
         <Navbar.Toggle id="nav-toggler-icon" />
-        <Navbar.Collapse >
+        <Navbar.Collapse>
           <Nav className="ml-auto nav-menu-items">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/#portfolio">Portfolio</Nav.Link>
             <Nav.Link href="/#skills">Skills</Nav.Link>
-            <Nav.Link href="/#contact" >Contact</Nav.Link>
+            <Nav.Link href="/#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     </section>
-  )
-}
+  );
+};
