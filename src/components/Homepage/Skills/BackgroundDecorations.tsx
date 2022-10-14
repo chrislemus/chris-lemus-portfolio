49fa4e5264d442f51ee45b8c79350f41fb0c2569
chrislemus <icrislemus@gmail.com>
2022-10-14 00:46:23 -0400

    refactor and implement typescript in Skils component

---
+++ src/components/Homepage/Skills/BackgroundDecorations.tsx
@@ -0,0 +1,34 @@
+import { useParallaxShift } from '@root/src/hooks';
+import styles from './BackgroundDecorations.module.scss';
+
+export const BackgroundDecorations = () => {
+  const offset = useParallaxShift();
+
+  return (
+    <>
+      <img
+        src="./images/decorations/water-splash.svg"
+        alt="water-splash"
+        className={styles.decorationWater}
+      />
+      <img
+        src="./images/decorations/sm-circle.svg"
+        alt="circle"
+        className={styles.decorationCircle}
+        style={{ bottom: offset / 20 }}
+      />
+      <img
+        src="./images/decorations/sm-square.svg"
+        alt="square"
+        className={styles.decorationSquare}
+        style={{ bottom: offset / 60 }}
+      />
+      <img
+        src="./images/decorations/sm-half-circle.svg"
+        alt="half-circle"
+        className={styles.decorationHalfCircle}
+        style={{ bottom: offset / 5 }}
+      />
+    </>
+  );
+};
